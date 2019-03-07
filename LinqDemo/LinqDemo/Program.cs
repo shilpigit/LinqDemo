799900180d7c916a5e672d26fe1b8aa9dc5f5e56
shilpigit <shilpi@virtualemployee.com>
2019-03-07 23:18:37 +0530

    added new linq example

--- LinqDemo/LinqDemo/Program.cs
+++ LinqDemo/LinqDemo/Program.cs
@@ -7,36 +7,96 @@ using System.Threading.Tasks;
 
 namespace LinqDemo
 {
+    class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            List<Department> DepList = new List<Department>()
+            {
+                new Department{ID=1,Name="IT" },
+                new Department{ID=2,Name="Admin" }
+            };
             List<Employee> empList = new List<Employee>();
-            empList.Add(new Employee() { ID=1,Name="Shilpi", Department="IT", Designation="B" });
-            empList.Add(new Employee() { ID = 2, Name = "Shilpi1", Department = "IT", Designation = "A" });
-            empList.Add(new Employee() { ID = 3, Name = "Shilpi2", Department = "IT", Designation = "B" });
-            empList.Add(new Employee() { ID = 4, Name = "Shilpi3", Department = "IT", Designation = "B" });
-            empList.Add(new Employee() { ID = 5, Name = "Shilpi4", Department = "IT", Designation = "C" });
+            empList.Add(new Employee() { ID=1,Name="Shilpi", Department="IT", Designation="B",DepartmentID=1 });
+            empList.Add(new Employee() { ID = 2, Name = "Shilpi1", Department = "IT", Designation = "A", DepartmentID = 1 });
+            empList.Add(new Employee() { ID = 3, Name = "Shilpi2", Department = "IT", Designation = "B", DepartmentID = 1 });
+            empList.Add(new Employee() { ID = 4, Name = "Shilpi3", Department = "IT", Designation = "B", DepartmentID = 2 });
+            empList.Add(new Employee() { ID = 5, Name = "Shilpi4", Department = "IT", Designation = "Management", DepartmentID = 2 });
+          
+            //empList.Insert(0,new Employee { })
 
-
+            string djfhjfd = 1.ToString();
             //var result = empList.Where(x => x.Designation == "B");
 
-            IEnumerable iE = empList.Where(x => x.Designation == "B").OrderBy(x=>x.Designation).Take(2);
+            IEnumerable iE = empList.Where(x => x.Designation == "B");//.OrderBy(x=>x.Designation).Take(2);
+
+            var test = (from emp in empList
+                        where emp.Designation == "B"
+                        select emp).AsQueryable();
+
+            if (true)
+            {
+                test = test.Take(2);
+            }
 
 
-            IQueryable iQ = empList.Where(x => x.Designation == "B").Take(2).OrderBy(x => x.Designation).AsQueryable();
+           var iQ = empList.Where(x => x.Designation == "B").Take(2).OrderBy(x => x.Designation).ToList();//.AsQueryable();
 
 
             var res = empList.Where(x => x.Designation == "B").ToList();
+
+            var test4 = empList.Where(x => x.Designation == "B").Select(x => x.Department);
+
+            var test5 = empList.Where(x => x.Designation == "B").Select(x => new EmpModel
+            {
+                EmpDepartment = DepList.Where(y=>y.ID==x.DepartmentID).FirstOrDefault().Name,
+                EmpDesignation = x.Designation,
+                EmpID=x.ID,
+                EmpName=x.Name
+            }).ToList();
+
+            //from linq
+
+            var test6 = from emp in empList
+                        join dep in DepList on emp.DepartmentID equals dep.ID
+                        select new EmpModel
+                        {
+                            EmpDepartment = dep.Name,
+                            EmpDesignation = emp.Designation,
+                            EmpID = emp.ID,
+                            EmpName = emp.Name
+                        };
+            var test7 = empList.Select(x =>
+              {
+                  if (x.Designation == "Management")
+                  {
+                      x.CustomText = "Welcome Sir,";
+                  }
+                  return x;
+              });
+
             var anyRes = empList.Any(x => x.Name == "Shilpi");
         }
     }
-
+    class EmpModel
+    {
+        public int EmpID;
+        public string EmpName;
+        public string EmpDepartment;
+        public string EmpDesignation;
+    }
     class Employee
     {
         public int ID;
         public string Name;
         public string Department;
+        public int DepartmentID { get; set; }
         public string Designation;
+        public string CustomText { get; set; }
     }
 }
---
+++ LinqDemo/Linqdemo2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqdemo2
+{
+    class Program
+    {
+        public class StudentModel
+        {
+            public int studentID { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int subID { get; set; }
+        }
+
+        public class SubjectModel
+        {
+            public int subjectID { get; set; }
+            public string SubjectName { get; set; }
+        }
+
+        public class subMappingModel
+        {
+            public string SubjectName { get; set; }
+            public string ExtraComment { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<subMappingModel> subMapping = new List<subMappingModel>();
+            List<SubjectModel> subDemo = new List<SubjectModel>()
+            {
+                new SubjectModel{subjectID=1, SubjectName="Maths"},
+                 new SubjectModel{subjectID=2, SubjectName="Physics"},
+                  new SubjectModel{subjectID=3, SubjectName="Chemistry"},
+                   new SubjectModel{subjectID=4, SubjectName="Biology"},
+                    new SubjectModel{subjectID=5, SubjectName="Botany"},
+                     new SubjectModel{subjectID=6, SubjectName="English"}
+            };
+
+            List<StudentModel> stuDemo = new List<StudentModel>()
+            {
+                new StudentModel{studentID=1, subID=1, Name="Shilpi", Age=30},
+                new StudentModel{studentID=2, subID=2, Name="Surjeet", Age=28},
+                new StudentModel{studentID=3, subID=3, Name="Naeem", Age=30},
+                new StudentModel{studentID=4, subID=4, Name="Anas", Age=22},
+                new StudentModel{studentID=5, subID=5, Name="Neha", Age=33},
+                new StudentModel{studentID=6, subID=6, Name="Rekhanshi", Age=23},
+                new StudentModel{studentID=7, subID=3, Name="Pushpa", Age=44},
+                new StudentModel{studentID=8, subID=2, Name="Anu", Age=34},
+                new StudentModel{studentID=9, subID=4, Name="Ashu", Age=22},
+                new StudentModel{studentID=10, subID=1, Name="Yogesh", Age=34},
+            };
+
+            var subName = stuDemo.Select(x => new subMappingModel
+            {
+                SubjectName = subDemo.Where(y => y.subjectID == x.subID).FirstOrDefault().SubjectName
+            }).ToList();
+
+            var subNameOne = stuDemo.Select(x => new subMappingModel
+            {
+                SubjectName = subDemo.Where(y => y.subjectID == x.subID).First().SubjectName
+            }).ToList();
+
+            foreach (var subItem in subNameOne)
+            {
+                Console.WriteLine(subItem.SubjectName);
+            }
+
+            var subExtraComment = subDemo.Select(x => new subMappingModel
+            {
+                SubjectName = x.SubjectName,
+                ExtraComment = subMapping.Where(y => y.SubjectName == "Chemistry" ? y.ExtraComment = "Great" : y.ExtraComment = null)
+            });
+        }
+    }
+}
